28701e3a3a6176602500d383714ca24baf7f4659
ElenaLuchnikova <159582817+ElenaLuchnikova@users.noreply.github.com>
2026-04-15 16:33:40 +0300

    Update Script(method#2).sql

--- Когортный анализ LTV/Script(method#2).sql
+++ Когортный анализ LTV/Script(method#2).sql
@@ -1,4 +1,4 @@
-with t1 as (
+with t_cohort as (
 	select
 		dt,
 		user,
@@ -18,13 +18,13 @@ with t1 as (
   max(diff) as max_diff,
   round(sum(case when diff=0 then sale end)/count(distinct user),2) as "0",
   case when max(diff)> 0 then round(sum(case when diff<=7 then sale end)/count(distinct user),2) end as "7",
-  case when max(diff)> 0 then round(sum(case when diff<=14 then sale end)/count(distinct user),2) end as "14",
-  case when max(diff)> 0 then round(sum(case when diff<=30 then sale end)/count(distinct user),2) end as "30",
+  case when max(diff)> 7 then round(sum(case when diff<=14 then sale end)/count(distinct user),2) end as "14",
+  case when max(diff)> 14 then round(sum(case when diff<=30 then sale end)/count(distinct user),2) end as "30",
   case when max(diff)>30 then round(sum(case when diff<=60 then sale end)/count(distinct user),2) end as "60",
   case when max(diff)>60 then round(sum(case when diff<=90 then sale end)/count(distinct user),2) end as "90",
   case when max(diff)>90 then round(sum(case when diff<=180 then sale end)/count(distinct user),2) end as "180",
   case when max(diff)>180 then round(sum(case when diff<=270 then sale end)/count(distinct user),2) end as "270",
   case when max(diff)>270 then round(sum(case when diff<=365 then sale end)/count(distinct user),2) end as "360"
-  from t1
+  from t_cohort
   group by cohort
   order by cohort
